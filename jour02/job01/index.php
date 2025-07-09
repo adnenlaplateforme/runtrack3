f63d3a9a8c1c6e29254bbc62606b9f7a6161a373
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-09 11:24:30 +0200

    job01 ok

---
+++ jour02/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job00</title>
+</head>
+
+<body id="body">
+    <article id="citation">
+        La vie a beaucoup plus d'imagination que nous
+    </article>
+    <button id="button">Button</button>
+    <button onclick="citation2()">Button2</button>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
---
+++ jour02/job01/script.js
@@ -0,0 +1,12 @@
+function citation() {
+  const text = document.getElementById('citation').textContent
+  const button = document.querySelector('#button')
+  button.addEventListener('click', (e) => {
+    console.log(text)
+  })
+}
+citation()
+function citation2() {
+  const text = document.getElementById('citation').textContent
+  console.log(text)
+}
