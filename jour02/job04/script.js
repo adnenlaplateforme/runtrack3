f65269ba8ae3b9dcd22455319db72f8bc34d7422
adnenlaplateforme <adnen.said@laplateforme.io>
2025-07-10 09:26:13 +0200

    jour02/job04 repair script

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -1,8 +1,7 @@
 document.addEventListener('keydown', (e) => {
   const textareaElem = document.getElementById('keylogger')
-  console.log(document.activeElement)
   if (document.activeElement.id === 'keylogger') {
     textareaElem.value = textareaElem.value + e.key
   }
-  textareaElem.innerText = textareaElem.value + e.key
+  textareaElem.value = textareaElem.value + e.key
 })
